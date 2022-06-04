b6be17ce50c5b62c007a6218f0c198a9dccd9004
Duong Dac <dacduong07@gmail.com>
2022-06-04 02:00:10 +0200

    Restructure

---
+++ main.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <complex>
+#include <iostream>
+#include <utility>
+#include <vector>
+
+typedef std::complex<double> cmplx;
+typedef std::vector<cmplx> jarray;
+const size_t DATA_SIZE = 1 << 18;
